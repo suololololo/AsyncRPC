2f07f9a311c6f4aab1565a533df1a896605ea220
suololololo <5533673+suololololo@user.noreply.gitee.com>
2023-01-19 17:11:44 +0800

    add function

--- src/utils.cc
+++ src/utils.cc
@@ -2,6 +2,7 @@
 #include "fiber.h"
 #include <unistd.h>
 #include <syscall.h>
+#include <sys/time.h>
 namespace RPC {
 pid_t GetThreadId() {
     return syscall(SYS_gettid);
@@ -11,5 +12,27 @@ pid_t GetThreadId() {
 uint64_t GetFiberId() {
     return Fiber::GetFiberId();
 }
+
+/**
+ * @brief 获取当前时间的毫秒数 (ms)
+ *  1秒= 1000 毫秒  1毫秒= 1000微秒
+ * @return uint64_t 
+ */
+uint64_t GetCurrentMS() {
+    struct timeval tm;
+    gettimeofday(&tm, 0);
+    return tm.tv_sec * 1000ul + tm.tv_usec / 1000;
+}
+/**
+ * @brief 获取当前时间的微秒数(us)
+ *  1秒= 1000 毫秒  1毫秒= 1000微秒
+ * @return uint64_t 
+ */
+uint64_t GetCurrentUS() {
+    struct timeval tm;
+    gettimeofday(&tm, 0);
+    return tm.tv_sec * 1000ul * 1000ul + tm.tv_usec;
+}
+
 }
 
